d20e72b5b25dda383e031d7de1760551e0089da8
mahdigharibzadeh7 <77227383+mahdigharibzadeh7@users.noreply.github.com>
2022-06-02 03:19:15 +0430

    Add files via upload

---
+++ TodoList/todo.js
@@ -0,0 +1,60 @@
+// Create a "close" button
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
+}
+
+
+// Click close button to remove
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function() {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+
+// Checked symbol
+var list = document.querySelector('ul');
+list.addEventListener('click', function(ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+
+// Create a new list item
+function newElement() {
+    var li = document.createElement("li");
+    var inputValue = document.querySelector(".input").value;
+    console.log(inputValue)
+    li.innerHTML=inputValue
+    console.log(li)
+    if (inputValue === '') {
+      alert("You must write something!");
+    } 
+    else {
+        
+        var span = document.createElement("SPAN");
+        var txt = document.createTextNode("\u00D7");
+        span.className = "close";
+        span.appendChild(txt);
+        li.appendChild(span);  
+        document.querySelector(".todo-list").appendChild(li);
+    }
+    document.querySelector(".input").value = "";
+  
+    for (i = 0; i < close.length; i++) {
+      close[i].onclick = function() {
+        var div = this.parentElement;
+        div.style.display = "none";
+      }
+    }
+}
